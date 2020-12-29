7f8718a1d9e3dfa64e9bd117978dde58ec3efcfa
TimeJerome <76664411+TimeJerome@users.noreply.github.com>
2020-12-29 03:11:15 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,40 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHtml(true);
+
+$mail->setFrom('televizorkuhonnyj9@gmail.com','Заказчик');
+
+$mail->addAddress('timejerome@gmail.com');
+
+$mail->Subject = 'Новый заказ';
+
+$body = '<h1>Новый Заказчик</h1>';
+
+if(trim(!empty($_POST['name']))){
+    $body.=<p><strong>Имя:</strong> '.$_POST['name'].'</p>;
+}
+if(trim(!empty($_POST['pfone']))){
+    $body.=<p><strong>Номер:</strong> '.$_POST['pfone'].'</p>;
+}
+$mail->Body = $body;
+
+if (!$mail->send()) {
+    $message = 'Ошибка';
+} else {
+    $message = 'Данные отправлены!';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+?>
+
